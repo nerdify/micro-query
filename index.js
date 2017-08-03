a6ab0cbddc222aff51879e504576ec066e394aa9
Hosmel Quintana <hosmelq@gmail.com>
2017-08-03 12:27:40 -0600

    update qs and remove prettier

--- index.js
+++ index.js
@@ -1,12 +1,12 @@
 const qs = require('qs');
 
-module.exports = (req) => {
-  let url = req.url;
-  const index = url.indexOf('?');
+module.exports = req => {
+	let url = req.url;
+	const index = url.indexOf('?');
 
-  if (index !== -1) {
-    url = url.substr(index + 1);
-  }
+	if (index !== -1) {
+		url = url.substr(index + 1);
+	}
 
-  return qs.parse(url);
-}
+	return qs.parse(url);
+};
